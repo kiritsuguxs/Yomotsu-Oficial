9cbe3a8d798e7616fec3afcf86d687c6f80b62e6
kiritsuguxs <semnada1999@gmail.com>
2026-09-17 01:31:24 +0000

    fix: Corrige import inválido do Material3 (*Button) gerado acidentalmente

--- app/src/main/java/eu/kanade/presentation/more/profile/UserProfileScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/profile/UserProfileScreen.kt
@@ -17,7 +17,15 @@ import androidx.compose.material.icons.outlined.CollectionsBookmark
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material.icons.outlined.MenuBook
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
