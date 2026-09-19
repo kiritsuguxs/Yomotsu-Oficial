a6082b601e9ac349d5b685354b9a5623ee5d5951
kiritsuguxs <semnada1999@gmail.com>
2026-09-19 15:53:31 +0000

    fix: add TransformOrigin import

--- app/src/main/java/eu/kanade/translation/presentation/SmartTranslationBlock.kt
+++ app/src/main/java/eu/kanade/translation/presentation/SmartTranslationBlock.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
